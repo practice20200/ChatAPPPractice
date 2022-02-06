017a07c8f16bec80bcd97dd911857cbc5b6e070b
practice20200 <wor9hard2018@gmail.com>
2022-02-06 11:10:27 -0800

    Initial Commit

---
+++ ChatAppPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatAppPractice
+//
+//  Created by Apple New on 2022-02-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
